1cf51e32e0219e470f4e7c86b5a3e8fe011e65fd
Eevan <eevan.redon@gmail.com>
2020-01-23 13:22:27 +0100

    add subscriptions controller

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,5 @@ module TheReactiveHeraldApi
           max_age: 0
       end
     end
-    config.stripe.secret_key = Rails.application.credentials.stripe[:secret_key]
-    config.stripe.publishable_key = Rails.application.credentials.stripe[:publishable_key]
   end
 end
