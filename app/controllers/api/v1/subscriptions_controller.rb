48e34e6f645420eba141c21d62ee6ac87c24e842
Eevan <eevan.redon@gmail.com>
2020-01-23 21:26:28 +0100

    add Stripe charge create method, updates role if payment success, renders error if not

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,12 +1,30 @@
 class Api::V1::SubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  rescue_from Stripe::InvalidRequestError, with: :invalid_token
   
   def create
     customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
+      email: current_user.email,
       source: params[:stripeToken]
     )
-    render json: customer
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      currency: 'sek',
+      amount: 500
+    )
+    render json: charge
+    
+    if (charge.paid)
+      current_user.role = 'subscriber'
+      current_user.save
+    end
   end
+  
+  private
+
+  def invalid_token 
+    render json: { error: "Transaction rejected, token invalid"}
+  end 
 
 end
--- spec/requests/api/v1/subscription_spec.rb
+++ spec/requests/api/v1/subscription_spec.rb
@@ -8,19 +8,44 @@ RSpec.describe 'USer can buy subscritption' do
   let(:user_credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
 
-  let(:successful_token) { post '/api/v1/subscriptions', params: {  stripeEmail: user.email,
+  let(:successful_token) { post '/api/v1/subscriptions', 
+    params: {
     stripeToken: stripe_helper.generate_card_token    
     }, headers: headers
-      user.reload }
+  user.reload }
 
-  describe "User pays for subscritption" do
+  let(:invalid_token) { post '/api/v1/subscriptions', 
+    params: {
+    stripeToken: 123456789
+    }, headers: headers}
+
+  describe "User pays for subscription" do
     before do
       successful_token
     end
 
-    it 'Successfully and becomes subscriber' do
+    it 'Successfully' do
       expect(response).to have_http_status 200
     end
 
+    it 'has payment approved' do
+      expect(response_json["paid"]).to eq true
+    end
+
+    it 'has their role updated to subscriber' do
+      expect(user.role).to eq 'subscriber'
+    end
+
   end
+
+  describe "User pays for subscription with invalid token" do
+    before do
+      invalid_token
+    end
+
+    it 'unsuccessfully' do
+      expect(response_json["error"]).to eq 'Transaction rejected, token invalid'
+    end
+  end
+
 end 
