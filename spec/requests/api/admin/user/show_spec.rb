329f1959861eb75ba9472abcbe0d9e5c87655a68
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-23 10:39:20 +0100

    Updates formatting using solargraph

--- spec/requests/api/admin/user/show_spec.rb
+++ spec/requests/api/admin/user/show_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 RSpec.describe 'GET/api/admin/users', type: :request do
   let(:user) { create(:user) }
   let(:user_credentials) { user.create_new_auth_token }
-  let!(:user_headers) {{ HTTP_ACCEPT: 'application/json' }.merge!(user_credentials)}
+  let!(:user_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
   let!(:non_authorized_headers) { { HTTP_ACCEPT: 'application/json' } }
 
   describe 'Succesfully show profile page' do
@@ -14,7 +16,7 @@ RSpec.describe 'GET/api/admin/users', type: :request do
     end
 
     it 'returns user email' do
-      expect(response_json["user"]["email"]).to eq (user.email)
+      expect(response_json['user']['email']).to eq user.email
     end
   end
 
