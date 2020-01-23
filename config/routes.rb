86774ccbc29a3147b4e594853bce0a22e312dd3a
mantanquzzmo <quzzmo@hotmail.com>
2020-01-23 11:27:05 +0100

    merges johans solargraph edit with removed extra route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
     namespace :v1, defaults: { format: :json } do
