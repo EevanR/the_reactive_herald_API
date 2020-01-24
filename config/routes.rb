f0775c0d6d76b492b0dd36e055ef624b316fd97b
Eevan <eevan.redon@gmail.com>
2020-01-24 10:53:14 +0100

    remove unecessary :new route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :articles, only: [:show, :index]
-      resources :subscriptions, only: [:new, :create]
+      resources :subscriptions, only: [:create]
       end
     namespace :admin, defaults: { format: :json } do
       resources :articles, only: [:create, :update, :index]
