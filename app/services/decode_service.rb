52ab75f172f9307791f8c0149ff321c5762723c9
oliverochman <oliver.ochman@hotmail.com>
2020-01-27 16:37:26 +0100

    change variabel name in decode_service

--- app/services/decode_service.rb
+++ app/services/decode_service.rb
@@ -12,14 +12,14 @@ module DecodeService
 
 	private
 
-	def self.split_base64(uri_string)
-		if uri_string =~ /^data:(.*?);(.*?),(.*)$/
-      uri = {}
-      uri[:type] = Regexp.last_match(1)
-      uri[:encoder] = Regexp.last_match(2)
-      uri[:data] = Regexp.last_match(3)
-      uri[:extension] = Regexp.last_match(1).split('/')[1]
-      uri
-    end
+	def self.split_base64(image_string)
+		if image_string =~ /^data:(.*?);(.*?),(.*)$/
+			uri = {}
+			uri[:type] = Regexp.last_match(1)
+			uri[:encoder] = Regexp.last_match(2)
+			uri[:data] = Regexp.last_match(3)
+			uri[:extension] = Regexp.last_match(1).split('/')[1]
+			uri
+		end
 	end
 end
