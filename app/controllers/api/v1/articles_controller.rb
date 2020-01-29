e10a0b1db958c4617fac777c684c7adc9dddf783
mantanquzzmo <quzzmo@hotmail.com>
2020-01-29 12:56:59 +0100

    moved excess line to make it DRY

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,11 +4,10 @@ class Api::V1::ArticlesController < ApplicationController
   def index
     if params[:category]
       articles = Article.where(category: params[:category]).paginate(page: params[:category], per_page: 4)
-      render json: articles, each_serializer: Articles::IndexSerializer, meta: meta_attributes(articles)
     else
       articles = Article.paginate(page: params[:page], per_page: 4)
-      render json: articles, each_serializer: Articles::IndexSerializer, meta: meta_attributes(articles)
     end
+    render json: articles, each_serializer: Articles::IndexSerializer, meta: meta_attributes(articles)
   end
 
   def show
