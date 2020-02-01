41596a7ba03e68f8a8bc470deafa2f28e24bc35e
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-01 16:38:50 +0100

    Adds request specs for delete action

---
+++ spec/requests/api/v1/admin/articles/delete_spec.rb
@@ -0,0 +1,65 @@
+RSpec.describe 'GET/api/admin/articles', type: :request do
+  let(:publisher)  { create(:publisher)}
+  let(:publisher_credentials) { publisher.create_new_auth_token }
+  let!(:publisher_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(publisher_credentials) }
+  let!(:article) { create(:article) }
+
+
+  describe 'Successfully lists unpublished articles' do
+    before do
+      delete "/api/v1/admin/articles/#{article.id}",
+      headers: publisher_headers
+    end
+    
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns success message' do
+      expect(response_json['message'].count).to eq "Article has been deleted"
+    end
+
+    it 'returns the article writer' do
+      get "/api/v1/articles/#{article.id}",
+      binding.pry
+      expect(response_json['errors']).to eq "Article not found"
+    end
+  end
+
+  describe 'unsuccessfully when' do
+    describe 'logged in as a journalist' do
+      let(:journalist) { create(:user, role: 'journalist')}
+      let(:journalist_credentials) { journalist.create_new_auth_token }
+      let!(:journalist_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(journalist_credentials) }
+      
+      before do
+        delete "/api/v1/admin/articles/#{article.id}",
+        headers: journalist_credentials
+      end
+
+      it 'returns a 404 response status' do
+        expect(response).to have_http_status 404
+      end
+
+      it 'returns error message' do
+        expect(response_json["error"]).to eq "Not authorized!"
+      end
+    end
+
+    describe 'not logged in' do
+      let!(:non_authorized_headers) { { HTTP_ACCEPT: 'application/json' } }
+      before do
+        get "/api/v1/admin/articles",
+        headers: non_authorized_headers
+      end
+      
+      it 'returns a 401 response status' do
+        expect(response).to have_http_status 401
+      end
+
+      it 'returns error message' do
+        expect(response_json["errors"][0]).to eq "You need to sign in or sign up before continuing."
+      end
+    end
+  end
+end
